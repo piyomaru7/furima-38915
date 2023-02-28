7d066044b71f49aec21fed2714d59c926eaae11c
piyomaru7 <kbtslugger14@gmail.com>
2023-02-28 15:34:56 +0900

    itemモデルのテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name             { Faker::Name.name }
+    explanation      { Faker::Lorem.sentence }
+    category_id      { 2 }
+    status_id        { 2 }
+    postage_id       { 2 }
+    area_id          { 2 }
+    number_of_day_id { 2 }
+    price            { 300 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/sample1.png'), filename: 'sample1.png')
+    end
   end
 end
