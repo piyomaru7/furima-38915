86ae4155f6ce5b12270439e42d14bbe629ceb08d
piyomaru7 <kbtslugger14@gmail.com>
2023-03-03 14:44:31 +0900

    商品購入機能の実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :zip_code, :area_id, :municipality, :street_number, :building_name, :telephone_number,
+                :order_id, :token
+
+  validates :zip_code,         presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+  validates :area_id,          presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :municipality,     presence: true
+  validates :street_number,    presence: true
+  validates :telephone_number, presence: true, format: { with: /\A[0-9]{10,11}\z/ }
+  validates :user_id,          presence: true
+  validates :item_id,          presence: true
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(zip_code: zip_code, area_id: area_id, municipality: municipality, street_number: street_number,
+                   building_name: building_name, telephone_number: telephone_number, order_id: order.id)
+  end
+end
