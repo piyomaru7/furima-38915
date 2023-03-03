b903d603c34fb1e73474163ff2a2a8c341d9a8e4
piyomaru7 <kbtslugger14@gmail.com>
2023-03-03 16:59:48 +0900

    order_addressモデルのテスト

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    zip_code         { '123-4567' }
+    area_id          { 2 }
+    municipality     { '金沢市' }
+    street_number    { '田中' }
+    building_name    { '金沢ハイツ' }
+    telephone_number { '08022002200' }
+  end
+end
